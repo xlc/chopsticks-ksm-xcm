bc1b7ade0dce5ecb95aee320fe5192c7bda72619
Bryan Chen <xlchen1291@gmail.com>
2023-11-10 11:21:02 +1300

    change the tx to one sending to karura

--- ksm.test.js
+++ ksm.test.js
@@ -1,14 +1,14 @@
 import { test } from 'vitest'
 import { setupNetworks, checkSystemEvents } from '@acala-network/chopsticks-testing'
 
-// replay this tx: https://kusama.subscan.io/extrinsic/20471678-2
+// replay this tx: https://kusama.subscan.io/extrinsic/0x605419331808783c03f7f16d76e912983a0db742574a50db53de8c88e7fec34d
 
 test('test', async () => {
     const { kusama, karura } = await setupNetworks({
     kusama: {
       endpoint: 'wss://kusama-rpc.polkadot.io',
       db: "./db.sqlite",
-      block: 20471677, // the parent of the block we want to replay
+      block: 20466524, // the parent of the block we want to replay
       port: 8000,
       "runtime-log-level": 5,
       'wasm-override': process.env.KUSAMA_WASM,
@@ -21,8 +21,8 @@ test('test', async () => {
     },
   })
 
-  // signed tx taken from 20471678-2
-  const tx = '0x69028400d2b4c334b6ee67c7f4f42a47efb4345978d4787514fd79dd6ded6cc73107265c01588e625a56b365ef1cf55f79bc9cf481ab2c46d9c56a91cb6ba503de873b4879909d557bb29b14b984cf2cdde937c0ffdf974076d2155304e328567c5a73888c6503a4006308010001009d1f0100010300f8b497a64b1216b8caaaadcc710b99d22d4b38d20104000000000b0040c2ab19150000000000'
+  // signed tx for txhash 0x605419331808783c03f7f16d76e912983a0db742574a50db53de8c88e7fec34d
+  const tx = '0x990284007c2371230cc4289cbdc36ac0880f435ddadb79a64545f2f0d30fcde2bce0c5010138fb5264333fe5b650d1490491fa4fbea4af8cf190dfc434c9cad8a3037c2e2f5cc7f649bd52bb9ed57cf85feeab09f9af8d35ad10f4dab69e1b5a4b9ab1d68595010d0600630803000100411f03000101007c2371230cc4289cbdc36ac0880f435ddadb79a64545f2f0d30fcde2bce0c5010304000000000740eb8ad3d90000000000'
 
   await kusama.api.rpc.author.submitExtrinsic(tx)
   await kusama.dev.newBlock()
